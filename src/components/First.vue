790105f43f48314418e39767aeea231f617a99d7
rvladimir001 <32569748+rvladimir001@users.noreply.github.com>
2021-04-29 14:30:40 +0300

    add files project event-bus-exmpl

---
+++ src/components/First.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>First component</h1>
+    <h1>Changed from second component - {{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "First",
+  data() {
+    return {
+      name: 'This from first component'
+    }
+  },
+  created() {
+    this.$eventHub.$on('change-from-second', this.changeName)
+  },
+  beforeDestroy() {
+    this.$eventHub.$off('change-from-second');
+  },
+  methods: {
+    changeName(name) {
+      this.name = name;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Second.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Secont component</h1>
+    <div>
+      <input type="text" v-model="newName">
+      <button @click.prevent="changeFromSecond()">Click!!</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Second",
+  data() {
+    return {
+      newName: ''
+    }
+  },
+
+  methods: {
+    changeFromSecond() {
+      this.$eventHub.$emit('change-from-second', this.newName);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
